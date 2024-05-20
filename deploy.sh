d962675698f23b6eb32f052a5afa6d46c89abe83
waleo <walea.ojo@gmail.com>
2024-05-20 15:26:25 -0400

    new deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 Car application is ready to be deployed to the development environment.
 Car application deployment must be reviewed.
 Car application development must be reviewed by managers before deployment.
+Engineers at work
