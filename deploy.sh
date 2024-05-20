8d2216978bdc4e9e164ee637b62a4aacae0229b2
kodjoss <Borla.ojo@gmail.com>
2024-05-20 18:32:34 +0000

    updated deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 Car application is ready to be deployed to the development environment.
-Car app deployment must be reviewed
-Car app development must be reviewed by managers before deployment.
+Car application deployment must be reviewed.
+Car application development must be reviewed by managers before deployment.
